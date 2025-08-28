a437b3a7674d5846dd016c31b377873f8caa35c1
Tiago Soares <tiago.soares@nortrez.com>
2025-08-27 21:35:08 -0300

    feat(): add atv5.js

---
+++ primeirobi/atv5.js
@@ -0,0 +1,8 @@
+const valorConta = 100;
+const percentualGorgeta = 15;
+
+const valorGorgeta = valorConta * (percentualGorgeta / 100)
+
+const valorAPagar = valorConta + valorGorgeta
+
+console.log(valorAPagar)
